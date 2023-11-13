9c60d47561f0445413da7af9b5fb7ad0a79cb3c6
Kelly Singh <k225sing@uwaterloo.ca>
2023-11-13 16:25:20 -0500

    working! kinda

--- ant_colony.py
+++ ant_colony.py
@@ -6,6 +6,11 @@ import payoffs
 import main
 import numpy as np
 
+def normalize_2d(matrix):
+    norm = np.linalg.norm(matrix)
+    matrix = matrix/norm  # normalized matrix
+    return matrix
+
 def ant_colony():
     # initalizing a random head nodes, and all nodes that can be used in the crawl optimization
     head, nodes = main.create()
@@ -32,18 +37,22 @@ def ant_colony():
             ant_crawl = crawl_class.Crawl([]) # initialize an empty crawl per ant
             probability = random.random()   # probability for ant to visit a bar
             for t in range(hours):
-                for bar in range(len(nodes)):
+                for bar in range(len(nodes)): # TODO: make random somehow
                     limit = phermones[bar][t]   # chance of visiting the bar is based on the phermones
                     if (probability < limit):   # if probability is less than the limit, go to the bar
-                        bar_chosen = phermones[bar][t] 
+                        bar_chosen = bar
+                        time_chosen = t
                         break
                     else:
                         probability -= limit   # increase the probability of going to a bar if the bar limit is too high
 
                 # add bar stop to the path the ant is on
-                ant_crawl.append(bar_chosen)
+                if (ant_crawl.stops == []):
+                    prev_stop_endtime = 0
+                else:
+                    prev_stop_endtime = ant_crawl.stops[-1].e_time
+                ant_crawl.append(classes.stop(nodes[bar_chosen], (prev_stop_endtime)//60, (prev_stop_endtime//60)+time_chosen))
             
-            print(ant_crawl)
             # evaluate the ant's path to calculate payoff
             crawl_payoff = ant_crawl.evaluate_crawl()  
 
@@ -53,11 +62,11 @@ def ant_colony():
                 best_crawl = ant_crawl
             
             # updating phermones
-            for stop in ant_crawl:
-                    phermones[stop.node][stop.e_time-stop.s_time] += best_payoff/gamma
+            for stop in ant_crawl.stops:
+                    phermones[nodes.index(stop.node)][int((stop.e_time-stop.s_time)/60)] += best_payoff/gamma
 
             # normalize the phermones in the bar
-            phermones = np.norm(phermones)
+            phermones = normalize_2d(phermones)
 
     return best_crawl
 
