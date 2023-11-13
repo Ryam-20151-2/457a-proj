4be2bffda9109b7cd54b8a533611db8b438eb438
Kelly Singh <k225sing@uwaterloo.ca>
2023-11-13 15:46:55 -0500

    is it our fault? idk

--- ant_colony.py
+++ ant_colony.py
@@ -13,22 +13,22 @@ def ant_colony():
     t = 0 # iteration for ant colony algorithm
     n = 1000 # number of ants
     hours = 8 # length of crawl in hours
-    p = 0.5 # hyperparameter
+    gamma = 2000 # hyperparameter
     time_elapsed = np.zeros(n) # time elapsed for each ant, initially zero
 
     # creating a 2D array to hold the phermones with the bars and the times, initally zero
     phermones = np.ones((len(nodes), hours))
     # change first column to be zeros EXCEPT at the head
-    for i in range(len(nodes)):
-        if (i != csv.index(head)):  # TODO: why the fuck is this index not working
-            phermones[i][0] = 0
-    
+    for bar in nodes:
+        if (bar != head): 
+            phermones[nodes.index(bar)][0] = 0
+
     # initalize optimized values
     best_crawl = crawl_class.Crawl([])
     best_payoff= 0
 
     while(t<100): # until convergence...
-        for ant in range(len(n)):
+        for ant in range(n):
             ant_crawl = crawl_class.Crawl([]) # initialize an empty crawl per ant
             probability = random.random()   # probability for ant to visit a bar
             for t in range(hours):
@@ -43,22 +43,22 @@ def ant_colony():
                 # add bar stop to the path the ant is on
                 ant_crawl.append(bar_chosen)
             
+            print(ant_crawl)
             # evaluate the ant's path to calculate payoff
-            number = ant_crawl.evaluate_crawl   # TODO: does this number mean our payoff per path??
-            concat_crawl = ant_crawl # TODO: concatenate the crawl
+            crawl_payoff = ant_crawl.evaluate_crawl()  
 
             # check the best crawl so far and update accordingly
-            if (number > best_payoff):
-                best_payoff = number
-                best_crawl = concat_crawl
+            if (crawl_payoff > best_payoff):
+                best_payoff = crawl_payoff
+                best_crawl = ant_crawl
             
             # updating phermones
-            # TODO: too much brain rn but have todo a for for the bars visited
-            # then add the payoff/hyperparameter to the phermone for that bar
+            for stop in ant_crawl:
+                    phermones[stop.node][stop.e_time-stop.s_time] += best_payoff/gamma
 
             # normalize the phermones in the bar
             phermones = np.norm(phermones)
 
     return best_crawl
 
-                    
+ant_colony()
