13b7fab4476422e0a7a6bb892cbd0b5a7467c1e6
Megan Gooch <mgooch@uwaterloo.ca>
2023-11-18 16:14:25 -0500

    debugging norm and random prob

--- ant_colony.py
+++ ant_colony.py
@@ -6,15 +6,14 @@ import numpy as np
 
 def normalize_2d(matrix):
     r, c = np.shape(matrix)
+    c_sum = np.sum(matrix, axis=0) # list of each column's summed elements
     for i in range(0, c):
-        sum = np.sum(matrix[:, i])
         for j in range(0, r):
-            matrix[j, i] /= sum
-
+            matrix[j][i] /= c_sum[i]
     return matrix
 
 def choose_row(vector, prob):
-    for i in range(0, len(vector)):
+    for i in range(0, len(vector)): 
         if (prob <= vector[i]):
             return i
         else:
@@ -31,73 +30,50 @@ def ant_colony(head, nodes, num_iter=100, ants_per_iter=50):
     for bar in nodes:
         if (bar != head): 
             phermones[nodes.index(bar), 0] = 0
+    
+    phermones = normalize_2d(phermones)
 
     # initalize optimized values
     best_crawl = crawl_class.Crawl([])
     best_payoff= 0
     total_payoff= 0
 
-
-    phermones = normalize_2d(phermones)
-
-    pass
-
     for k in range(0, num_iter):
-
-        crawl_list = []
-        crawl_list_eval = []
+        crawl_list = [] # list of crawl objects that all the ants go to (each ant does 1 crawl each - comparing the ant's crawls)
+        crawl_list_eval = [] # list of crawl object payoffs from each ant's crawl
         for i in range(0, ants_per_iter):
-            crawl_list.append(crawl_class.Crawl([]))
+            crawl_list.append(crawl_class.Crawl([])) # initalizing empty crawl per ant
         
         stop_index = []
         for ant in crawl_list:
-
-            r_num = random.random()   # probability for ant to visit a bar
-            stop_index_ant = []
+            stop_index_ant = [] # list of all the stops that the ant goes to the crawl as indicies refering to the order of nodes
             
-            for c in range(hours):
-                #random.shuffle(nodes) # randomly iterate through bars so the first bar isnt always chosen first
-
-                row = phermones[:, c]
-                index = choose_row(vector=row, prob=r_num)
+            for c in range(0, hours):
+                r_num = random.random()   # probability for ant to visit a bar (between 0 and 1)
+                row = phermones[:, c] # all the element in column number 'c'
+                index = choose_row(vector=row, prob=r_num) # index of the node (i.e. bar) we want to go to
                 stop_index_ant.append(index)
-                node = nodes[index]
+                node = nodes[index] # node object the ant is going to
 
-                stop = classes.stop(node=node, s_time=(c*60), e_time=((c+1)*60))
+                stop = classes.stop(node=node, s_time=(c*60), e_time=((c+1)*60)) # convert to minutes
                 ant.append(stop=stop)
 
-                # for bar in range(len(nodes)): 
-                #     limit = phermones[bar][c]   # chance of visiting the bar is based on the phermones
-                #     if (probability < limit):   # if probability is less than the limit, go to the bar
-                #         bar_chosen = bar
-                #         time_chosen = c
-                #         break
-                #     else:
-                #         probability -= limit   # increase the probability of going to a bar if the bar limit is too high
-
-                # add bar stop to the path the ant is on
-                # if (ant_crawl.stops == []):
-                #     prev_stop_endtime = 0
-                # else:
-                #     prev_stop_endtime = ant_crawl.stops[-1].e_time
-                # ant_crawl.append(classes.stop(nodes[bar_chosen], (prev_stop_endtime)//60, (prev_stop_endtime//60)+time_chosen))
-            
             # evaluate the ant's path to calculate payoff
             ant.concatenate()
             crawl_list_eval.append(ant.evaluate_crawl())
-            stop_index.append(stop_index_ant)
+            stop_index.append(stop_index_ant) # keeps track of the nodes the ants visited (to avoid updating nodes that ants didn't go to)
 
-        maxVal = max(crawl_list_eval)
-        minVal = min(crawl_list_eval)
-        maxIndex = crawl_list_eval.index(maxVal)
+        maxVal = max(crawl_list_eval) # max payoff
+        minVal = min(crawl_list_eval) # min payoff
+        maxIndex = crawl_list_eval.index(maxVal) # getting the index of the max payoff
 
-        if maxVal > best_payoff:
+        if maxVal > best_payoff: # updating the best
             best_crawl = crawl_list[maxIndex]
             best_payoff = maxVal
 
         avgPayoff = sum(crawl_list_eval) / len(crawl_list_eval)
         delta_payoff = maxVal - minVal
-        if delta_payoff == 0:
+        if delta_payoff == 0: # avoid diving by zero in first iteration
             delta_payoff = 0.1
 
         # updating phermones
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import main
 import ant_colony
 
 head, nodes = main.create()
-crawl = ant_colony.ant_colony(head, nodes)
+crawl = ant_colony.ant_colony(head, nodes, 1000, 1000)
 crawl.concatenate()
 print(crawl)
 print(crawl.evaluate_crawl())
